e9256b332c6a9c4c9c0ccd5f34ec0cdfa92ee7eb
Paulo Henrique Reis <phdsreis@gmail.com>
2025-09-29 15:04:42 -0300

    feat: seção 13 - pacotes externos

---
+++ 13_pacotes/notes.go
@@ -0,0 +1,19 @@
+package main
+
+func main() {
+	// =-=-=-=-=-=-==--=-=-=-=-= pacotes =-=-=-=-=-=-==--=-=-=-=-=
+
+	/*
+		exemplo de instalação de um pacote externo
+		
+		1. inicia o go mod
+		-> go mod init <nome do módulo>
+		
+		2. installar o pacote
+		-> go get github.com/fatih/color (exemplo)
+		
+		3. baixar as dependecias
+		-> go get
+		-> (ou) go mod tidy
+	*/
+}
